fa07bf003c5c867203f145ad68c973b7109fd876
KereszturiHanna <kerhanna05@gmail.com>
2025-11-12 09:04:03 +0100

    Függvények 2. feladat

---
+++ Függvények/feladat2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>📝 2. feladat: Tömb függvények gyakorlása</h1>
+    <p>Hozz létre egy tömböt, ami 5-6 kedvenc ételed nevét tartalmazza.
+    Készíts egy függvényt, ami:
+    visszaadja a tömb hosszát (hány elem van benne),
+    hozzáad egy új ételt a tömbhöz,
+    rendezi a tömböt ábécé szerint,
+    és visszaadja a teljes tömböt.
+    Cél: tömbök kezelése, elemek hozzáadása, rendezés, függvények használata tömbökkel.</p>
+
+
+    <?php
+
+        $etelek = array("Gyros", "Bolognai spagetti", "Húsos palacsinta", "Szendvics", "Rakott csirke");
+
+        function etelInfo($etelek){
+            $elemekSzama = count($etelek);  //visszaadja a tömb hosszát (hány elem van benne)
+            $ujTetel = "Töltött káposzta";
+            $etelek[] = $ujTetel;  //hozzáad egy új ételt a tömbhöz
+            $abc = sort($etelek); //rendezi a tömböt ábécé szerint
+
+
+            return[
+                "elemekSzama" => $elemekSzama,
+                "ujTetel" => $ujTetel,
+                "abc" => $etelek, //és visszaadja a teljes tömböt.
+            ];
+        }
+
+        $kaja = etelInfo($etelek);
+        echo 
+        "<p>Elemek száma: " . $kaja["elemekSzama"] . "</p>" . 
+        "<p>Új tétel: " . $kaja["ujTetel"] . "</p>". 
+        "<p>Abc sorrend: " .  implode(' , ',$kaja["abc"]) . "</p>";
+
+    ?>
+
+
+</body>
+</html>
