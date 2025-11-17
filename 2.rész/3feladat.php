f01a116661bf4b886e78c51ec3297b3529e7eba2
KereszturiHanna <kerhanna05@gmail.com>
2025-11-17 11:05:52 +0100

    2.feladat

---
+++ 2.rész/3feladat.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>3) Egyszerű saját függvények gyakorlása (paraméter + visszatérési érték)</h1>
+    <p>Feladat:
+    Hozz létre 3 különböző feladatot, ahol saját függvényt használnál:<br>
+    1. Egy függvény, ami kap egy árat és visszaadja a 27% áfás árat
+    → Használj paramétert és visszatérési értéket.<br>
+    2. Függvény, ami megkap egy nevet, és egy random köszönést küld vissza<br>
+    3. Például: „Szia Márk, remélem jó napod lesz!” — de legyen mindig más.<br>
+    Függvény, ami kap egy számot és eldönti róla, hogy páros vagy páratlan</p>
+
+<hr>
+    <form method="POST">
+        <legend>
+            <label for="arNev">Ár:</label>
+            <input type="text" name="ar" required>
+        </legend>
+        <br>
+        <button type="submit">Küldes</button>
+    </form>
+
+    <?php
+
+        function Afa($ar){
+            $afasAr = $ar * 1.27;
+            return number_format($afasAr,0, " ");
+        }
+
+        if (isset($_POST['ar']) && $_POST['ar'] !== '') {
+
+            $ar = $_POST['ar'];
+            $AfaInfo = Afa($ar);
+    
+            echo "<p>$ar ÁFA-s ára: $AfaInfo Ft</p>";  
+        } 
+    ?>
+<hr>
+
+    <form method="POST">
+        <legend>
+            <label for="nev">Név:</label>
+            <input type="text" name="nev" required>
+        </legend>
+        <br>
+        <button type="submit">Küldes</button>
+    </form>
+
+    <?php
+
+        function Udvozles($nev){
+            return "Szia ". $nev.", remélem jó napod lesz!";
+        }
+
+        if (isset($_POST['nev']) && $_POST['nev'] !== '') {
+
+            $nev = $_POST['nev'];
+            $UdvozlesInfo = Udvozles($nev);
+            echo $UdvozlesInfo;
+      
+        } 
+    ?>
+
+
+</body>
+</html>
