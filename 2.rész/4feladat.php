44c24d5f021600da549538b005c61b5d7aa48570
KereszturiHanna <kerhanna05@gmail.com>
2025-11-18 10:19:07 +0100

    4.feladat

---
+++ 2.rész/4feladat.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tömb + saját függvény combo</title>
+</head>
+<body>
+    <h1>4) Tömb + saját függvény combo</h1>
+    <p>
+    Képzeld el, hogy van egy számlista: 12, 7, 30, 44, 3, 19...
+    Feladat:
+    Hozz létre 2 db saját függvényt:
+    ami kiszámolja a tömb átlagát
+    ami visszaad egy új tömböt, amiben csak a 10 feletti számok vannak
+    A feladatban írd le, hogy mikor hívod meg őket és mire használod az eredményt.
+    </p>
+
+    <?php
+        $szamok = array(12, 7, 30, 44, 3, 19, 21, 26, 40); //
+    ?>
+    
+</body>
+</html>
